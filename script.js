7a7a613997b2b50eda110d301eb23c5f13ddd7c2
cvelasquezpalacio <cvelasquezpalacio@gmail.com>
2026-05-09 20:26:46 -0500

    feat: convertir en pwa

--- script.js
+++ script.js
@@ -110,5 +110,12 @@ function copiar() {
         boton.classList.remove("copiado");
       }, 2000);
     });
+  
+  if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./service-worker.js")
+    .then(() => console.log("Service Worker registrado"))
+    .catch((error) => console.log("Error:", error));
+    }  
 }
 
